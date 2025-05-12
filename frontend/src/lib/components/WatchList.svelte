87ba7df09c398372bd57d09b50f34375d363323d
Chavez Harris <codedbychavez@gmail.com>
2025-05-11 20:31:21 -0400

    Init

--- frontend/src/lib/components/WatchList.svelte
+++ frontend/src/lib/components/WatchList.svelte
@@ -2,10 +2,18 @@
 	import MovieCard from './MovieCard.svelte';
 </script>
 
-<div class="watch-list py-4">
+<div class="watch-list py-4 h-full">
 	<h2 class="text-2xl text-center">My Watch List</h2>
 
-	<div class="movie-cards">
-		<!-- <MovieCard /> -->
+	<div class="columns flex justify-between mt-8 bg-teal-500 h-full">
+		<div class="col-1 border-r border-gray-200">
+			col-1
+		</div>
+		<div class="col-2 border-r border-gray-200">
+			col-2
+		</div>
+		<div class="col-3 border-r border-gray-200">
+			col-3
+		</div>
 	</div>
 </div>
