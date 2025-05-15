24427139f7dabea7add118f296150bc6be306dc1
Chavez Harris <codedbychavez@gmail.com>
2025-05-15 19:22:12 -0400

    Add home page

---
+++ frontend/src/lib/components/CallToAction.svelte
@@ -0,0 +1,53 @@
+<div class="cta">
+	<h3>Take Control of Your Watchlist</h3>
+	<p>
+		Say goodbye to scattered movie notes. Plan, sort, and track what you want to watch—all in one
+		board.
+	</p>
+	<button>Start Watching Smarter</button>
+</div>
+
+<style>
+	.cta {
+		height: max-content;
+		padding: 3rem 0;
+		max-width: 750px;
+    margin: 0 auto;
+	}
+
+	.cta h3 {
+		font-size: 2rem;
+		font-weight: 700;
+	}
+
+	.cta p {
+		margin-top: 1rem;
+	}
+
+	.cta button {
+		cursor: pointer;
+		margin-top: 3rem;
+		background-image: linear-gradient(to right, #fe8c00 0%, #f83600 51%, #fe8c00 100%);
+		padding: 15px 45px;
+		text-align: center;
+		text-transform: uppercase;
+		transition: 0.5s;
+		background-size: 200% auto;
+		color: white;
+		box-shadow: 0 0 20px #eee;
+		border-radius: 10px;
+		display: block;
+	}
+
+	.cta button:hover {
+		background-position: right center; /* change the direction of the change here */
+		color: #fff;
+		text-decoration: none;
+	}
+
+  @media (max-width: 768px) {
+  .cta {
+    padding: 2rem;
+  }
+}
+</style>
---
+++ frontend/src/lib/components/Hero.svelte
@@ -0,0 +1,48 @@
+<div class="hero">
+  <div class="text-container">
+    <h2>Organize Your Watchlist</h2>
+    <p>A visual watchlist built for film lovers. From "To Watch” to “Done," keep your movies in check.</p>
+  </div>
+</div>
+
+<style>
+
+.hero {
+  min-height: 60vh;
+  padding: 2rem;
+  background-color: #ffae13;
+  display: flex;
+  align-items: center;
+}
+
+.text-container {
+  margin: 0 auto;
+  max-width: 750px;
+}
+
+.text-container h2 {
+  /* font-family: 'Poppins'; */
+  font-size: 6rem;
+  font-weight: 900;
+}
+
+.text-container p {
+  margin-top: 2rem;
+  font-size: 20px;
+}
+
+/* Media Queries */
+
+@media (max-width: 768px) {
+  .text-container h2 {
+    font-size: 3.5rem;
+  }
+}
+
+@media (max-width: 420px) {
+  .text-container h2 {
+    font-size: 2.8rem;
+  }
+}
+
+</style>
